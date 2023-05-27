66170f914b1cd01319beeb72673a8578caece2b0
Bidhu1024 <bidhu1024av@gmail.com>
2023-05-27 14:06:32 +0530

    Reducer

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
-const CAKE_ORDERED = "CAKE_ORDERED"
+const CAKE_ORDERED = "CAKE_ORDERED";
 
-const orderCake=()=>{
-    return{
+const orderCake = () => {
+  return {
     type: CAKE_ORDERED,
-    quantity:1,
-}
-}
-
+    quantity: 1,
+  };
+};
+const initialState = {
+  numOfCakes: 10,
+};
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case CAKE_ORDERED:
+      return {
+        ...state,
+        numOfCakes: state.numOfCakes - 1,
+      };
+    default:
+      return state;
+  }
+};
